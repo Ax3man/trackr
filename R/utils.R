5b904f47406f1011f06fa4a91e0c18306bffd538
Ax3man <wouter.van.der.bijl@zoologi.su.se>
2016-03-31 10:29:35 +0200

    Export magrittr pipe (%>%)

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,7 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
 get_party_df_names <- function(d) {
   fun <- function(x) names(eval(x))
   multidplyr::cluster_call(d$cluster, fun, as.name(d$name))[[1]]
