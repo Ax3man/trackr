c326d7e8da71aa0ea95366479e695ac393b0ea34
Ax3man <wouter.van.der.bijl@zoologi.su.se>
2016-06-02 17:23:53 +0200

    speed and related calculations are now in seconds instead of frames.

--- R/add_to_tr.R
+++ R/add_to_tr.R
@@ -11,6 +11,9 @@
 #' @param order_by This parameter controls the ordering of \code{dplyr::lag} and
 #'   \code{dplyr::lead} and it is strongly advised to leave it at it's default
 #'   (\code{frame}).
+#' @param frame_rate All variables with a time dimension, will be calculated
+#'   based on seconds (not frames). By default this is read from the tracks
+#'   object.
 #'
 #' @name mutate_tr
 #' @seealso mutate_soc mutate_.tracks
@@ -18,7 +21,8 @@ NULL
 
 #' @rdname mutate_tr
 #' @export
-change <- function(x, order_by = frame, n = 1) {
+change <- function(x, order_by = frame,
+                   frame_rate = tracks$params$frame_rate, n = 1) {
   ifelse(order_by - dplyr::lag(order_by, n = n, order_by = order_by) == n,
          x - dplyr::lag(x, n = n, order_by = order_by),
          NA)
@@ -26,14 +30,16 @@ change <- function(x, order_by = frame, n = 1) {
 
 #' @rdname mutate_tr
 #' @export
-speed <- function(x = X, y = Y, order_by = frame) {
-  sqrt(change(x, order_by) ^ 2 + change(y, order_by) ^ 2)
+speed <- function(x = X, y = Y, order_by = frame,
+                  frame_rate = tracks$params$frame_rate) {
+  sqrt(change(x, order_by, frame_rate) ^ 2 + change(y, order_by, frame_rate) ^ 2)
 }
 
 #' @rdname mutate_tr
 #' @export
-acceleration <- function(x = X, y = Y, order_by = frame) {
-  change(speed(y, x, order_by), order_by)
+acceleration <- function(x = X, y = Y, order_by = frame,
+                         frame_rate = tracks$params$frame_rate) {
+  change(speed(y, x, order_by, frame_rate), order_by, frame_rate)
 }
 
 #' @rdname mutate_tr
