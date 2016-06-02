677d9160c8d67fe2996d2e6431dbe66dd050e160
Ax3man <wouter.van.der.bijl@zoologi.su.se>
2016-06-02 10:52:36 +0200

    Ironed out small bugs. Now uses join_tr_to_soc_.

--- R/pairwise_correlations.R
+++ R/pairwise_correlations.R
@@ -30,11 +30,14 @@ calc_speed_lag <- function(tracks, range = 100, time_bin = NULL) {
   if (!('speed' %in% tracks$pr$tr)) {
     stop('Speed not found in tr table.', call. = FALSE)
   }
-
+  if (is.null(tracks$soc)) {
+    tracks <- expand_tracks(tracks, group = FALSE, trial = FALSE,
+                            animal = FALSE, pair = FALSE)
+  }
   multidplyr::cluster_assign_value(tracks$soc$cluster, 'range', range)
   multidplyr::cluster_assign_value(tracks$soc$cluster, 'find_max_cross_corr',
                                    find_max_cross_corr)
-  tracks <- join_tr_to_soc(tracks, list(~speed))
+  tracks <- join_tr_to_soc_(tracks, ~speed)
 
   if (is.null(time_bin)) {
     tracks$soc <- dplyr::mutate_(tracks$soc, time_bin = ~1)
@@ -47,12 +50,14 @@ calc_speed_lag <- function(tracks, range = 100, time_bin = NULL) {
   tracks$soc <- dplyr::group_by_(tracks$soc, ~animal1, ~animal2, ~time_bin)
 
   res <- dplyr::do_(tracks$soc, ~find_max_cross_corr(.$speed1, .$speed2, range))
-  res <- dplyr::filter_(res, !is.na(time_bin))
-
+  if (!is.null(time_bin)) {
+    res <- dplyr::filter_(res, ~!is.na(time_bin))
+  }
   res <- dplyr::collect(res)
   if (is.null(time_bin)) {
     res <- dplyr::select_(res, ~-time_bin)
   }
+
   return(res)
 }
 
