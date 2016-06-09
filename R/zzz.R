0fb6b4ab41dfafe9124e169a2eb1a298cfcee586
Ax3man <wouter.van.der.bijl@zoologi.su.se>
2016-06-09 15:54:53 +0200

    Further improved startup messages

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,14 @@
 .onAttach <- function(...) {
-  sapply(strwrap(
-    "Welcome to trackr. \n\nPlease note that the package is currently in development, and code may break at any time. Please file issues and feature suggestions on the GitHub page (github.com/Ax3man/trackr/issues).",
-    indent = 0, exdent = 1), packageStartupMessage)
+  if (!interactive()) {
+    return()
+  }
+  msgs <- c(
+    "Please note that the package is currently in development, and code may break at any time.",
+    "Please file issues and feature suggestions on the GitHub page (github.com/Ax3man/trackr/issues)",
+    "To see the user guides use `browseVignettes('trackr')`"
+  )
+  msg <- sample(msgs, 1)
+  packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
 }
 
 if (getRversion() >= "2.15.1") {
